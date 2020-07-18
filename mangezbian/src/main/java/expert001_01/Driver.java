add29ad8b6b93772e57f970bf72c7518491ad8b7
GitaekLee <coolfamily77@gmail.com>
2020-07-19 01:03:48 +0900

    - add import => spring framework package - add expert_001_01.xml => spring framework xml bean - edit => expert001_01/Driver.java
    @non-testing code

--- mangezbian/src/main/java/expert001_01/Driver.java
+++ mangezbian/src/main/java/expert001_01/Driver.java
@@ -1,10 +1,24 @@
 package expert001_01;
 
+// add import => spring framework package
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
 public class Driver {
 	
 	public static void main(String[] args) {
-		Tire tire = new KoreaTire();
-		Car car = new Car();
+		
+		ApplicationContext context = new FileSystemXmlApplicationContext("/src/main/java/expert001_01/expert001_01.xml");
+		//ApplicationContext context = new ClassPathXmlApplicationContext("expert001_01.xml", Driver.class);
+		
+		//ApplicationContext context = new ClassPathXmlApplicationContext("expert001_01/expert001_01.xml");
+
+		Car car = context.getBean("car", Car.class); 
+		
+		Tire tire = context.getBean("tire", Tire.class);
+
+		
 		car.setTire(tire);
 		
 		System.out.println(car.getTireBrand());
